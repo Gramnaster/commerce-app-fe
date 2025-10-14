837e327cc1312363718e74d6453326745c5cb3bc
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-14 15:38:00 +0800

    Routes: Basic Routes work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { Error, Home, Login, Signup } from './pages/index.ts';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error />,
+    children: []
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+    errorElement: <Error />
+  },
+  {
+    path: '*',
+    element: <Error />
+  }
+])
+
 function App() {
   return (
-    <>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export {default as Error} from "./ZZMisc/Error";
+export {default as Login} from "./Login/Login";
+export {default as Signup} from "./Signup/Signup";
+export {default as Home} from "./Home/Home";
