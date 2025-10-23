5a38e4355fcde0e80d5b4c720d823f33e197fddf
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-23 15:23:06 +0800

    Images: Renamed exports

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,33 +3,33 @@ export { default as cardMastercard } from './card-mastercard-01.png';
 export { default as cardPaypal } from './card-paypal-01.png';
 export { default as cardVisa } from './card-visa-01.png';
 
-export { default as ImageMainCategoryAirstrikes } from './cat-airstrikes-01.png';
-export { default as ImageMainCategoryChildrenClothes } from './cat-childrenclothes-01.png';
-export { default as ImageMainCategoryElectronics } from './cat-electronics-01.png';
-export { default as ImageMainCategoryJewellery } from './cat-jewellery-01.png';
-export { default as ImageMainCategoryMenClothes } from './cat-menclothes-01.png';
-export { default as ImageMainCategorySnacks } from './cat-snacks-01.png';
-export { default as ImageMainCategoryTrinkets } from './cat-trinkets-01.png';
-export { default as ImageMainCategoryWomenClothes } from './cat-womenclothes-01.png';
+export { default as MainCategoryAirstrikes } from './cat-airstrikes-01.png';
+export { default as MainCategoryChildrenClothes } from './cat-childrenclothes-01.png';
+export { default as MainCategoryElectronics } from './cat-electronics-01.png';
+export { default as MainCategoryJewellery } from './cat-jewellery-01.png';
+export { default as MainCategoryMenClothes } from './cat-menclothes-01.png';
+export { default as MainCategorySnacks } from './cat-snacks-01.png';
+export { default as MainCategoryTrinkets } from './cat-trinkets-01.png';
+export { default as MainCategoryWomenClothes } from './cat-womenclothes-01.png';
 
-export { default as ImageSmallCategoryAirstrikes } from './categories-08.png';
-export { default as ImageSmallCategoryChildrenClothes } from './categories-03.png';
-export { default as ImageSmallCategoryElectronics } from './categories-05.png';
-export { default as ImageSmallCategoryJewellery } from './categories-04.png';
-export { default as ImageSmallCategoryMenClothes } from './categories-01.png';
-export { default as ImageSmallCategorySnacks } from './categories-06.png';
-export { default as ImageSmallCategoryTrinkets } from './categories-07.png';
-export { default as ImageSmallCategoryWomenClothes } from './categories-02.png';
+export { default as SmallCategoryAirstrikes } from './categories-08.png';
+export { default as SmallCategoryChildrenClothes } from './categories-03.png';
+export { default as SmallCategoryElectronics } from './categories-05.png';
+export { default as SmallCategoryJewellery } from './categories-04.png';
+export { default as SmallCategoryMenClothes } from './categories-01.png';
+export { default as SmallCategorySnacks } from './categories-06.png';
+export { default as SmallCategoryTrinkets } from './categories-07.png';
+export { default as SmallCategoryWomenClothes } from './categories-02.png';
 
-export { default as ImageMainLogoDark } from './commerce-fe-main-logo-02-dark.png';
-export { default as ImageMainLogoLight } from './commerce-fe-main-logo-02-light.png';
-export { default as ImageSmallLogo } from './commerce-fe-small-logo-01.png';
+export { default as MainLogoDark } from './commerce-fe-main-logo-02-dark.png';
+export { default as MainLogoLight } from './commerce-fe-main-logo-02-light.png';
+export { default as SmallLogo } from './commerce-fe-small-logo-01.png';
 
-export { default as ImageFeaturedProduct01 } from './featured-product-01.png';
-export { default as ImageFeaturedProduct02 } from './featured-product-02.png';
-export { default as ImageFeaturedProduct03 } from './featured-product-03.png';
-export { default as ImageFeaturedProduct04 } from './featured-product-04.png';
-export { default as ImageFeaturedProduct05 } from './featured-product-05.png';
+export { default as FeaturedProduct01 } from './featured-product-01.png';
+export { default as FeaturedProduct02 } from './featured-product-02.png';
+export { default as FeaturedProduct03 } from './featured-product-03.png';
+export { default as FeaturedProduct04 } from './featured-product-04.png';
+export { default as FeaturedProduct05 } from './featured-product-05.png';
 
 export { default as IconCart } from './icon-cart-01.png';
 export { default as IconHamburger } from './icon-hamburger.png';
@@ -38,9 +38,9 @@ export { default as IconLineWhite } from './icon-line-light.png';
 export { default as IconProfile } from './icon-profile-01.png';
 export { default as IconSearch } from './icon-search-01.png';
 
-export { default as ImageSocial01 } from './social-01.png';
-export { default as ImageSocial02 } from './social-02.png';
-export { default as ImageSocial03 } from './social-03.png';
-export { default as ImageSocial04 } from './social-04.png';
+export { default as Social01 } from './social-01.png';
+export { default as Social02 } from './social-02.png';
+export { default as Social03 } from './social-03.png';
+export { default as Social04 } from './social-04.png';
 
 
