47650b8b269e4c53ebad3f827c0638cdfac0f720
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-26 19:11:10 +0800

    Fix: Adjsuted PopularCategories name to be more apt for its purpose

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,11 @@
 // import { Outlet } from "react-router-dom"
-
 import { FooterCTA } from "../../components"
-import Categories from "./Categories"
 import FeaturedProducts from "./FeaturedProducts"
 import FeaturedSocials from "./FeaturedSocials"
 import NewProducts from "./NewProducts"
 import { useSelector } from "react-redux"
 import type { RootState } from "../../store"
+import PopularCategories from "./PopularCategories"
 
 const Dashboard = () => {
   const user = useSelector((state: RootState) => state.userState.user);
@@ -14,7 +13,7 @@ const Dashboard = () => {
   return (
     <section>
       <FeaturedProducts />
-      <Categories />
+      <PopularCategories />
       <NewProducts />
       <FeaturedSocials />
       {/* If logged in, this CTA does not appear anymore */}
